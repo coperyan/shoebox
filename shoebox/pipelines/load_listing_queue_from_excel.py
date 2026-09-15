aa1cbb238dd7bccac093544de586b379a16bf275
Ryan Cope <rcope.data@gmail.com>
2026-09-15 05:35:02 -0700

    Update load_listing_queue_from_excel.py

--- shoebox/pipelines/load_listing_queue_from_excel.py
+++ shoebox/pipelines/load_listing_queue_from_excel.py
@@ -19,7 +19,9 @@ def get_input_df():
     settings = get_settings()
     input_path = Path(settings.paths.data_dir) / "Inputs (Param).xlsm"
     df = pd.read_excel(
-        input_path, sheet_name="Inputs", dtype={"Image Front": str, "Image Back": str}
+        input_path,
+        sheet_name="Inputs",
+        dtype={"Card #": str, "Image Front": str, "Image Back": str},
     )
     missing_images = df[df["Image Front"].isnull() | df["Image Back"].isnull()]
     if len(missing_images) > 0:
@@ -34,7 +36,9 @@ def get_input_df():
 
 def write_enriched_json(df: pd.DataFrame):
     settings = get_settings()
-    export_path = Path(settings.paths.exports_dir) / "jsonl/listing_queue_enriched.jsonl"
+    export_path = (
+        Path(settings.paths.exports_dir) / "jsonl/listing_queue_enriched.jsonl"
+    )
     cdf, pdf = load_checklist_and_parallels()
     enriched_rows = []
     for _, row in df.iterrows():
@@ -44,7 +48,9 @@ def write_enriched_json(df: pd.DataFrame):
                     set_name=row["Set Name"],
                     subset_name=row["Subset Name"],
                     parallel_variety=(
-                        row["Parallel/Variety"] if not pd.isnull(row["Parallel/Variety"]) else None
+                        row["Parallel/Variety"]
+                        if not pd.isnull(row["Parallel/Variety"])
+                        else None
                     ),
                     card_number=row["Card #"],
                     quantity=row["Quantity"],
@@ -65,5 +71,7 @@ def create_queue_file():
     logger.info(
         "Wrote %d records to the enriched jsonl:\n%s",
         len(df),
-        df[["Set Name", "Subset Name", "Parallel/Variety", "Card #"]].to_string(index=False),
+        df[["Set Name", "Subset Name", "Parallel/Variety", "Card #"]].to_string(
+            index=False
+        ),
     )
